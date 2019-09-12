27bb1c1e250558bc05ab59a04c095d7f8131a803
AlaRduTP <alardutp37259418@gmail.com>
2019-09-12 08:51:25 +0800

    tab -> 4 spaces

--- server.py
+++ server.py
@@ -5,22 +5,22 @@ SB = simple_bot()
 
 @SB.webhook
 def webhook():
-	pass
+    pass
 
 
 @SB.regex(r'foo\d+')
 def foo():
-	msg = {
-		'type': 'text',
-		'text': 'foo!!'
-	}
-	return msg
+    msg = {
+        'type': 'text',
+        'text': 'foo!!'
+    }
+    return msg
 
 
 @SB.keywds(['bar', 'rab'])
 def bar():
-	msg = {
-		'type': 'text',
-		'text': 'abr!!'
-	}
-	return msg
+    msg = {
+        'type': 'text',
+        'text': 'abr!!'
+    }
+    return msg
